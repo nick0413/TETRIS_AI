aa4f717fb269df1f2f9dbdc2bb5e4a4dcde87384
nick0413 <68749910+nick0413@users.noreply.github.com>
2024-01-21 12:17:49 -0500

    Line cleared bug corrected-new bug appeared
    So, while doing the for loop to check every piece individually, one may delete an piece with no blocks. This will cause the for loop to skip an item, so the Piece.empty was created to signify a piece should be deleted without doing it right there.
    
    Now, while deleting multiple lines, all the grounded pieces disappear. Further investigation is needed

--- tetris_engine.py
+++ tetris_engine.py
@@ -5,6 +5,7 @@ import time
 from lists import *
 import random
 import sys
+from typing import List
 random.seed(0)
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -36,7 +37,7 @@ def draw_grid(grid):
 		for line in lines:
 			line.draw()
 
-class piece:
+class Piece:
 
 
 	def __init__(self,type, starting_position, board_size) -> None:
@@ -48,6 +49,7 @@ class piece:
 		self.shape = codes_pieces[type][self.rotation]
 		self.board_size = board_size
 
+		self.empty: bool = False
 
 		self.max_x=max(self.shape[:,0])
 		self.min_x=min(self.shape[:,0])
@@ -118,25 +120,27 @@ class Tetris_board:
 		self.grounded_grid = np.copy(self.base_grid)
 		self.future_grid = np.copy(self.base_grid)
 		self.grid_drawing = define_grid(n,m, size,x_offset,y_offset)
-		self.pieces =[]
+		self.pieces: List[Piece] = []
 		self.square_size = size
 		self.selected_piece = None
 
 		self.x_offset = x_offset
 		self.y_offset = y_offset
 
+		
+
 		self.remaining_pieces = [1,2,3,4,5,6,7]
 
 	def start_game(self,starting_height: int = 22):
 		self.add_piece(
-			piece(self.Generate_next_piece(),(5,starting_height),self.base_grid.shape)
+			Piece(self.Generate_next_piece(),(5,starting_height),self.base_grid.shape)
 			)
 
 	def draw(self)->None:
 		draw_grid(self.grid_drawing)
 		self.draw_pieces()
 
-	def add_piece(self, piece: piece):
+	def add_piece(self, piece: Piece):
 		self.pieces.append(piece)
 		self.selected_piece = len(self.pieces)-1
 
@@ -189,7 +193,6 @@ class Tetris_board:
 			self.future_grid[piece.board_position()] = piece.type
 			result, flag = add_arrays_no_intersection(self.future_grid, self.grounded_grid)
 			if flag:
-				# print('intersection')
 				piece.grounded = True
 				self.intersection = True
 				piece.y-=direction
@@ -212,26 +215,31 @@ class Tetris_board:
 			return 
 		except IndexError:
 			if self.selected_piece < 0 or self.selected_piece > len(self.pieces):
-				print("Invalid piece index.")
+				print("Invalid piece index.-----------")
+				print("Selected piece:", self.selected_piece)
+				print("Pieces on the board:", len(self.pieces))
 				raise
 
 		if axis == "x" and self.check_within_bounds(self.pieces[self.selected_piece], axis, direction):
 			self.pieces[self.selected_piece].x += direction
 			self.base_grid=self.base_grid*0
 			self.base_grid[self.pieces[self.selected_piece].board_position()] = self.pieces[self.selected_piece].type
+		try:
+			if axis == "y" and self.check_within_bounds(self.pieces[self.selected_piece], axis, direction):
+				self.pieces[self.selected_piece].y += direction
+				self.base_grid=self.base_grid*0
+				self.base_grid[self.pieces[self.selected_piece].board_position()] = self.pieces[self.selected_piece].type
+		except IndexError:
+			print(len(self.pieces),self.selected_piece)
+			print("Error-line229")
 
-		if axis == "y" and self.check_within_bounds(self.pieces[self.selected_piece], axis, direction):
-			self.pieces[self.selected_piece].y += direction
-			self.base_grid=self.base_grid*0
-			self.base_grid[self.pieces[self.selected_piece].board_position()] = self.pieces[self.selected_piece].type
 
 	def check_piece_grounded(self, starting_height: int ):
 
-		
 		if self.pieces[self.selected_piece].grounded:
 			self.grounded_grid[self.pieces[self.selected_piece].board_position()]=self.pieces[self.selected_piece].type
 			r_int=self.Generate_next_piece()
-			self.add_piece(piece(r_int,(5,starting_height),self.base_grid.shape))
+			self.add_piece(Piece(r_int,(5,starting_height),self.base_grid.shape))
 			return True
 		self.check_lost()
 		self.check_line_cleared()
@@ -245,34 +253,63 @@ class Tetris_board:
 			self.start_game()
 
 	def check_line_cleared(self):
+		# for piece in self.pieces:
+		# 	print(piece)
+		# time.sleep(0.1)
 		lines_with_no_zeros = np.where(np.all(self.grounded_grid[:,:20] != 0, axis=0))[0]
-		# line_cleared = False
+		line_cleared = False
 		
 		if lines_with_no_zeros.size != 0:
-			print(lines_with_no_zeros)
+			print("lines cleared",lines_with_no_zeros)
+			pieces_removed=0
+			line_cleared = True
 			for piece in self.pieces:
-				print(piece.type, piece.board_position(True))
+
 				indices = piece.board_position(True)
 				piece_y = indices[:,1]
+
+				remaining_blocks=np.empty(0,dtype=int)
+				print(piece.type,'-----------------------------------')
 				print(piece_y)
 				if np.any(np.isin(piece_y, lines_with_no_zeros)):
 					remaining_blocks = np.where(~np.isin(piece_y, lines_with_no_zeros))
+
 					piece.shape = piece.shape[remaining_blocks]
 					line_cleared = True
 
-					# chekc if the piece.shape is empty
 					if piece.shape.size == 0:
-						self.pieces.remove(piece)
-						print('piece removed')
-						break
-		self.grounded_grid [:,lines_with_no_zeros] = 0
+						piece.empty = True
+						print('piece removed', piece.type)
 
+				indices = piece.board_position(True)
+				piece_y = indices[:,1]
+				if np.any(piece_y > np.max(lines_with_no_zeros)):
+					print(piece.shape)
+					print('piece moved down')
+					print(piece_y)
+					drop_blocks=np.where(piece_y > np.max(lines_with_no_zeros))
+					piece.shape[:,1][drop_blocks] -= len(lines_with_no_zeros)
+					print(piece.board_position(True))
 
+			for piece in self.pieces:
+				if piece.empty:
+					self.pieces.remove(piece)
+					pieces_removed+=1
+			self.selected_piece=len(self.pieces)-1
 
+			self.recalculate_ground()
 
-		# if (line_cleared):
-		# 	np.savetxt('array.csv' , np.flipud((self.grounded_grid).T), delimiter=',', fmt='%d')	
+		# if line_cleared:
+		# 	print(np.flipud((self.grounded_grid[:,:6]).T))
+			
 		# 	sys.exit()
+		clear_terminal()
+		print(np.flipud((self.grounded_grid+self.base_grid).T))
+
+
+
+
+
 
 
 	
@@ -287,6 +324,15 @@ class Tetris_board:
 	
 	def rotate_piece(self):
 		self.pieces[self.selected_piece].rotate()
+
+	def recalculate_ground(self):
+		self.grounded_grid=self.base_grid*0
+		for piece in self.pieces:
+			if piece.grounded:
+				print(piece.board_position(True),piece.type)
+				self.grounded_grid[piece.board_position()]=piece.type
+
+		np.savetxt("last_ground.csv", np.flipud(self.grounded_grid).T, fmt='%d', delimiter=",")
 
 
 
